664dc4cfb7389d8e67b75d5e01e1deb78855d909
mutation <mutation@naver.com>
2022-04-19 14:27:51 +0900

    0419 추가

---
+++ 0419/src/sum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class sum {
+
+	public static void main(String[] args) {
+
+		Scanner sc= new Scanner(System.in);
+		int a,b=0;
+		int sum=0;
+		System.out.println("시작 숫자: ");
+		a = sc.nextInt();
+		System.out.println("마지막 숫자: ");
+		b = sc.nextInt();
+		for(int i = a; i<=b;i++) {
+			sum += i;
+			
+		}
+		System.out.println(a+"부터"+b+"까지의 합은"+sum);
+	}
+	
+}
